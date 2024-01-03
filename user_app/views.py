da284adc394061ead83dbc3639f8aea69d9ce828
David Cortes <alexis241916@gmail.com>
2024-01-03 10:14:08 -0500

    Se realiza lo que podria ser el crud de crear, toca validar si funciona pero primero crear el super admin y validar por si funcion por hay, junto con la BD si esta creando los usuarios

--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Users
 
 # Create your views here.
 def formView(request):
     return render(request, "./login/login.html", {})
+
+def loginSignOn(request):
+    if request.method != "POST":
+       return render(request, "./login/login.html", {})
+    else:
+        try:
+            new_user= Users.objects.create(
+                name= request.POST["first_name"],
+                last_name= request.POST["last_name"],
+                birthday= request.POST["date_birthday"],
+                rol= request.POST["rol_user"],
+                email= request.POST["email"],
+                password= request.POST["password"],
+            )
+            return render(render, "")
+            #tambien le falta una url al que enviarse  
+        
+        except Exception as e:
+            error_message= f"Error al registrar usuario: {e}"
+            return render(request, "./login/login.html", {'error_message': error_message})
+            #el mensaje de error debe ir en el html de register  
+
+    return render(request, "") 
