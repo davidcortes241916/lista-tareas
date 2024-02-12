f96b45060161474b3fb3ca3630d97accbfdc5830
David Cortes <alexis241916@gmail.com>
2024-02-11 21:18:13 -0500

    Se realiza la arquitectura base de html de crear tareas, se crea la arquitectura base del metodo de create_Task donde toca complementarlo con la informacion, se crea un pequeño cambio asignando una id al boton de tasks.html, el metodo no va a transformar la tarea en pdf como se planea, que guarde y cree la tarea normalmente, el pdf ira solo para el manager al final para que pueda ver los empleados que estan registrados en una tabla en pdf y excel(si es posible)

--- tareas_app/views.py
+++ tareas_app/views.py
@@ -4,13 +4,27 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ValidationError
-#from .models import Users
+from .models import Tasks
 from django.db import close_old_connections, IntegrityError
 from django.urls import reverse
+#libreria para pdfs
+from django.views.generic import ListView, View
 
 # Create your views here.
 def list_Task(request):
     return render(request, "./home/tasks.html", {})
 
 def create_Task(request):
-    return render(request,"./tasks/create.html",{})
+    if request.method != "POST":
+        return render(request, "./tasks/create.htm",{})
+    else:
+        if "cancelar" in request.POST:
+            return redirect("./home/tasks.html")
+
+        try:
+            print("intentando crear tarea")
+            required_fields= []
+            pass
+
+        except Exception as e: 
+            pass
